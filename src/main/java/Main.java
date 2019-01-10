40127557f00a071d615c051eaa278fd49d635579
Andrew Li <thesorin65@gmail.com>
2019-01-09 19:11:18 -0600

    correctly added imports for wpilib libraries

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
+import java.awt.image.*;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args)throws Exception
     {
+        System.out.print("Hello World!");
         GripPipeline pipeline = new GripPipeline();
-
-        
+        BufferedImage image = ImageIO.read(new File("Images/Sample Panel.png"));
+        pipeline.process(bufferedImageToMat(image));
+        //MatOfPoint[] points = (MatOfPoint[])(pipeline.filterContoursOutput().toArray());
     }
+    public static Mat bufferedImageToMat(BufferedImage bi) {
+        Mat mat = new Mat(bi.getHeight(), bi.getWidth(), CvType.CV_8UC3);
+        byte[] data = ((DataBufferByte)bi.getRaster().getDataBuffer()).getData();
+        mat.put(0, 0, data);
+        return mat;
+      }
 }
