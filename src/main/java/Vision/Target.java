078a15b024202650fece2c9f9299123f817c1cfd
ujjainidas <43097770+ujjainidas@users.noreply.github.com>
2019-01-13 16:20:55 -0600

    Code to determine ratio of heights of half-targets in a pair

--- src/main/java/Vision/Target.java
+++ src/main/java/Vision/Target.java
@@ -1,15 +1,40 @@
 package Vision;
 
+import static org.junit.Assert.assertNotNull;
+
+import javax.lang.model.util.ElementScanner6;
+
 import org.opencv.core.*;
 
 public class Target {
     HalfTarget left, right;
     Point center = new Point();
+    HalfTarget largerTarget, smallerTarget;
+    double differenceRatio;
 
     public Target(HalfTarget l, HalfTarget r) {
         left = l;
         right = r;
         center.x = (left.center.x+right.center.x)/2;
         center.y = (left.center.y+left.center.y)/2;
+        if(left.height > right.height)
+        {
+            largerTarget = left;
+            smallerTarget = right;
+        }
+        else if(left.height < right.height)
+        {
+            largerTarget = right;
+            smallerTarget = left;    
+        }
+        else
+        {
+            largerTarget = smallerTarget = null;
+        }
+
+
+        differenceRatio = (largerTarget != null)? (smallerTarget.height/largerTarget.height) : 1.00;
+            
     }
+
 }
