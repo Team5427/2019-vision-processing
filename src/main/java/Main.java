eebcd4758fab2df13bedbc5c9e83764cbf4590b9
Akshat Jain <respectthegame23@gmail.com>
2019-01-20 22:05:28 -0600

    Revert "Revert "Vision Processing Distance and Angle" (#2)"
    This reverts commit bc4d9f9e44bc59f73d74592ef434e48377dacbb6.

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.awt.image.*;
+import java.io.File;
+import java.util.ArrayList;
+
+import Vision.VisionFrame;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        VisionFrame frame = new VisionFrame();
+    }
+}
